7b7c712c1de632f66e7ca3e6d13efbffea6efe15
Erik Moqvist <erik.moqvist@gmail.com>
2019-05-27 20:51:54 +0200

    Fix host build.

--- make/packages/curl-7.65.0.mk
+++ make/packages/curl-7.65.0.mk
@@ -2,6 +2,11 @@ LIBS += curl
 
 packages: $(PACKAGES)/curl-7.65.0
 
+ifeq ($(ARCH), arm)
+CURL_CPPFLAGS = CPPFLAGS="-I$(BUILD)/root/include"
+CURL_LDFLAGS = LDFLAGS="-L$(BUILD)/root/lib"
+endif
+
 $(PACKAGES)/curl-7.65.0: $(PACKAGES)/openssl-1.1.1b
 $(PACKAGES)/curl-7.65.0: $(ML_SOURCES)/curl-7.65.0.tar.xz
 	@echo "Building curl-7.65.0."
@@ -10,7 +15,9 @@ $(PACKAGES)/curl-7.65.0: $(ML_SOURCES)/curl-7.65.0.tar.xz
 	tar xf $(ML_SOURCES)/curl-7.65.0.tar.xz && \
 	cd curl-7.65.0 && \
 	./configure \
-	    CFLAGS="-ffunction-sections -fdata-sections -I$(BUILD)/root/include -L$(BUILD)/root/lib" \
+	    CFLAGS="-ffunction-sections -fdata-sections" \
+	    $(CURL_CPPFLAGS) \
+	    $(CURL_LDFLAGS) \
 	    --prefix=$(BUILD)/root $(ML_AUTOTOOLS_HOST) \
 	    --with-ssl --without-zlib --disable-shared --disable-manual \
 	    --disable-ftp --disable-ldap --disable-telnet --disable-dict \
