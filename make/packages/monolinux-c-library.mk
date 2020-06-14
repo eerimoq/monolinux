45e07605b897e5e8817e45744f864ac32440fce5
Erik Moqvist <erik.moqvist@gmail.com>
2020-06-14 21:36:56 +0200

    Monolinux logfile conf.

--- make/packages/monolinux-c-library.mk
+++ make/packages/monolinux-c-library.mk
@@ -1,5 +1,6 @@
 LIBS += ml
 LIBML = $(BUILD)/root/lib/libml.a
+MONOLINUX_C_LIBRARY_LOG_OBJECT_TXT ?= "\"/tmp/log_object.txt\""
 
 $(eval $(call PACKAGE_template,monolinux-c-library,$(MONOLINUX_C_LIBRARY_RSYNC),$(MONOLINUX_C_LIBRARY_BUILD),$(LIBML)))
 
@@ -8,7 +9,9 @@ $(MONOLINUX_C_LIBRARY_BUILD): $(MONOLINUX_C_LIBRARY_RSYNC)
 	echo "Building monolinux-c-library."
 	mkdir -p $(SYSROOT)/lib
 	$(MAKE) -C $(PACKAGES_DIR)/monolinux-c-library library \
-	    CC=$(CROSS_COMPILE)gcc ML_ROOT=.
+	    CC=$(CROSS_COMPILE)gcc ML_ROOT=. \
+	    CFLAGS_EXTRA="-DML_LOG_OBJECT_TXT='$(MONOLINUX_C_LIBRARY_LOG_OBJECT_TXT)'"
+
 	$(MAKE) -C $(PACKAGES_DIR)/monolinux-c-library install \
 	    ML_ROOT=. PREFIX=$(SYSROOT)
 	touch $@
